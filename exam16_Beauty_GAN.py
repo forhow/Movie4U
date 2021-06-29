38498865061b08c1e3040c167be9b546b59d2fcd
forhow <forhow@naver.com>
2021-06-29 11:34:12 +0900

    first commit

---
+++ exam16_Beauty_GAN.py
@@ -0,0 +1,31 @@
+import cv2
+import dlib
+
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+# import tensorflow.compat.v1 as tf
+# tf.disable_v2_behavior()
+
+
+'''
+dlib 모델 다운로드
+- https://github.com/davisking/dlib-models
+
+참고 https://webnautes.tistory.com/1410
+
+'''
+print(cv2.__version__)
+print(dlib.__version__)
+
+
+
+# detector = dlib.get_frontal_face_detector()
+#
+# predictor = dlib.shape_predictor('models/shape_predictor_5_face_landmarks.dat')
+#
+# img = dlib.load_rgb_image('./imgs/12.jpg')
+# plt.figure(figsize=(16,18))
+# plt.imshow(img)
+# plt.show()
+
+
